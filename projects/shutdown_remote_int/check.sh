40765c35d2044aa0c64459bece5f5802e5a998fa
Ubuntu <ubuntu@ip-172-31-44-69.ap-south-1.compute.internal>
2023-08-25 18:44:27 +0530

    new project named shutdown

---
+++ projects/shutdown_remote_int/check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#int-
+ssh -i /home/ubuntu/new.pem -o StrictHostKeyChecking=no ubuntu@ec2-43-205-112-61.ap-south-1.compute.amazonaws.com 'sudo shutdown -h now'
+
+#int-3
+ssh -i /home/ubuntu/new.pem -o StrictHostKeyChecking=no  ubuntu@ec2-65-1-1-165.ap-south-1.compute.amazonaws.com 'sudo shutdown -h now'
+
+#int-4
+ssh -i /home/ubuntu/new.pem -o StrictHostKeyChecking=no  ubuntu@ec2-3-110-86-112.ap-south-1.compute.amazonaws.com 'sudo shutdown -h now'
+
