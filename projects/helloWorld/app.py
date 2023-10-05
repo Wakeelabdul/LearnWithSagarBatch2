20d0be22a1a5bbf9205de8e61770fc31b612b0c2
Mohammed Abdul Wakeel <134629244+Wakeelabdul@users.noreply.github.com>
2023-10-05 22:08:01 +0530

    Update app.py

--- projects/helloWorld/app.py
+++ projects/helloWorld/app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask! - Thank You again'
+    return 'Web App with Python Flask!'
 
 app.run(host='0.0.0.0', port=81)
